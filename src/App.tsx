3070f918902311db871190e0aff4d1e5ed3fcc23
Jimmy Blanck <blanckjm@gmail.com>
2025-01-30 10:38:17 -0500

    created landing page

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css';
+import { Button } from 'antd';
+import { Layout } from 'antd';
+import { Footer, Header } from 'antd/es/layout/layout';
+import { Content } from 'antd/es/layout/layout';
+import { Image } from 'antd';
+import { Flex } from 'antd';
+import Title from 'antd/es/typography/Title';
+import { ArrowRightOutlined } from '@ant-design/icons';
+
+function App() {
+  return (
+    <>
+      <Layout style={{ borderRadius: '20px' }}>
+        <Content>
+          <Flex wrap={true} gap="20px" justify="center" align="center">
+            <Flex vertical={true} justify="center" align="center" gap="10px">
+              <Title>Shrimply Bricks</Title>
+              <p>shrimpy shrimpy shrimpy</p>
+              <Flex justify="center" align="center" gap="small">
+                <Button type="primary" style={{ padding: '20px' }}>
+                  Get Free Instructions
+                  <ArrowRightOutlined></ArrowRightOutlined>
+                </Button>
+                <Button style={{ padding: '20px' }}>Learn More</Button>
+              </Flex>
+            </Flex>
+            <Image src="https://cdn.rebrickable.com/media/thumbs/mocs/moc-200176/471708.png/1000x800.png?1737139837.6408758"></Image>
+          </Flex>
+        </Content>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
