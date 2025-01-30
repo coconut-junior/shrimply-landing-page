57afb7745316c22a0032db9dbd55d3b8955c46d2
Jimmy Blanck <blanckjm@gmail.com>
2025-01-30 12:12:16 -0500

    created dockerfile

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,13 @@ function App() {
         <Content>
           <Flex wrap={true} gap="20px" justify="center" align="center">
             <Flex vertical={true} justify="center" align="center" gap="10px">
-              <Title>Shrimply Bricks</Title>
-              <p>shrimpy shrimpy shrimpy</p>
+              <Title>Build Your Dreams</Title>
+              <p style={{ maxWidth: '400px' }}>
+                Ever wanted to build something cool that wasn't an official LEGO
+                set? Shrimply Bricks makes it easy. We've got custom building
+                instructions for a whole variety of themes that you can
+                download. You won't find these anywhere else!
+              </p>
               <Flex justify="center" align="center" gap="small">
                 <Button type="primary" style={{ padding: '20px' }}>
                   Get Free Instructions
